1b9329c71da5acee47a4bcf3ac15570f6b761335
nMckenryan <n.mckenryan@gmail.com>
2024-12-02 15:29:43 +1100

    fix post

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { tvShowRouter } from "./routers/tv_show";
 
@@ -8,7 +7,6 @@ import { tvShowRouter } from "./routers/tv_show";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   tvShows: tvShowRouter
 });
 
