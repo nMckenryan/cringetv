4fcc67b2b119b7c35c2658f9de6e67e9be001c0e
nMckenryan <n.mckenryan@gmail.com>
2024-12-08 11:43:48 +1100

    Fixed build break on page profile

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { db } from "~/server/db";
 import UICard from "~/app/_components/UICard";
 import NotFound from "~/app/not-found";
-import ReviewList from "~/app/_components/reviews/ReviewList";
 
 export default async function ProfilePage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const user = await db.user.findUnique({ where: { id: params.id } });
+  const user = await db.user.findUnique({ where: { id: (await params).id } });
 
   if (!user) {
     return <NotFound />;
@@ -32,8 +31,6 @@ export default async function ProfilePage({
           {/* {user?.reviews} */}
         </p>
       </UICard>
-
-      <ReviewList />
     </main>
   );
 }
